cfabd38a0e33976bcd50d21c6175aa80872407ef
markcherepakhapython <mvky2004@gmail.com>
2020-10-14 19:46:06 +0300

    Add Hello, World

--- Hello/settings.py
+++ Hello/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'firstapp'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Hello.urls'
+ROOT_URLCONF = 'hello.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Hello.wsgi.application'
+WSGI_APPLICATION = 'hello.wsgi.application'
 
 
 # Database
--- Hello/wsgi.py
+++ Hello/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hello.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
 
 application = get_wsgi_application()
